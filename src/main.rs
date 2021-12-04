daed7537ead07fdc01d50e33104b800f9eb375f5
cLazyZombie <rusad96@gmail.com>
2021-12-04 22:24:45 +0900

    REPL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io::{self, BufRead, Write};
+
+use interpreter_rs::{lexer::Lexer, token::Token};
+
 fn main() {
-    interpreter_rs::lexer::Lexer::new("hello");
-    println!("Hello, world!");
+    print!(">> ");
+    let _ = io::stdout().lock().flush();
+
+    for line in io::stdin().lock().lines() {
+        let line = line.unwrap();
+        let mut lexer = Lexer::new(&line);
+
+        while let Some(tok) = lexer.next_token() {
+            println!("{:?}", tok);
+            if tok == Token::EOF {
+                break;
+            }
+        }
+        print!(">> ");
+        let _ = io::stdout().lock().flush();
+    }
 }
